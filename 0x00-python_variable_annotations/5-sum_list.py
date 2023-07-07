c35504d80e3d9bb1fb51e95373dc14daca0c9967
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2023-07-07 08:53:38 +0100

    new commit

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from typing import List
 """Type Checking"""
+from typing import List
+
+
 def sum_list(input_list: List[float]) -> float:
     """Return sum of list of floats
 
@@ -10,10 +12,4 @@ def sum_list(input_list: List[float]) -> float:
     Returns:
         float: sum of list of floats
     """
-    return sum(input_list)
-
-floats = [3.14, 1.11, 2.22]
-floats_sum = sum_list(floats)
-print(floats_sum == sum(floats))
-print(sum_list.__annotations__)
-print("sum_list(floats) returns {} which is a {}".format(floats_sum, type(floats_sum)))
+    return (sum(input_list))
