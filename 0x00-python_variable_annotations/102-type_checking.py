11e5bf407382924fb147b8b773fcae8a2fdcb32e
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2023-07-07 08:59:29 +0100

    new commit

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
-"""Type Checking"""
-from typing import Tuple
+""" Type Checking  """
+from typing import Tuple, List
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    """Returns a tuple"""
-    zoomed_in: Tuple = tuple([item for item in lst for i in range(factor)])
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ Zoom Array """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
     return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
